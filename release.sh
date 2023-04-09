6aa9a9e5ea90368df63cd9c842d7b22f348a199d
Vincent Young <missuo@pm.me>
2023-04-10 00:08:41 +0800

    fix: keep directory

--- release.sh
+++ release.sh
@@ -3,12 +3,12 @@
  # @Author: Vincent Young
  # @Date: 2023-04-09 23:56:13
  # @LastEditors: Vincent Young
- # @LastEditTime: 2023-04-10 00:00:09
- # @FilePath: /add-to-buyandship/releash.sh
+ # @LastEditTime: 2023-04-10 00:08:22
+ # @FilePath: /add-to-buyandship/release.sh
  # @Telegram: https://t.me/missuo
  # 
  # Copyright © 2023 by Vincent, All Rights Reserved. 
 ### 
 version=${1#refs/tags/v}
 mkdir dist
-zip -r -j dist/added-to-buyandship-$version.zip *
+zip -r dist/added-to-buyandship-$version.zip *
